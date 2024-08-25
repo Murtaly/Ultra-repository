637eb8618edc306a6f2768175f6526826d63ed46
Murtaly <137692483+Murtaly@users.noreply.github.com>
2024-08-25 15:27:27 +0300

    Add files via upload

---
+++ Memory_card Sanyochka/main.py
@@ -0,0 +1,5 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QApplication, QWidget, QTableWidget, QListWidget, QListWidgetItem, QLineEdit, 
+                             QFormLayout, QHBoxLayout, QVBoxLayout, QGroupBox, QButtonGroup, QRadioButton,
+                             QPushButton, QLabel, QSpinBox)
+from layout import *
