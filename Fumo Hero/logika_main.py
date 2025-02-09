f05e87b3c268e329199d7bd2e1f01621c36632f9
Oleksandr <137692483+Murtaly@users.noreply.github.com>
2025-02-09 15:32:23 +0200

    Add files via upload

--- Fumo Hero/logika_main.py
+++ Fumo Hero/logika_main.py
@@ -1,11 +1,13 @@
 from pygame import *
 import random
-mixer.init()
+import math
+
+mixer.init() 
 
 screen = display.set_mode((900, 720))
 display.set_caption("Fumo Hero")
 
-background = transform.scale(image.load("Fumo Hero/background_space.jpg"), (900, 720))
+background = transform.scale(image.load("background_space.jpg"), (900, 720))
 
 screen.blit(background, (0, 0))
 
@@ -13,15 +15,14 @@ win_width = 900
 win_height = 720
 Fumo_spawn_y = -65
 allbullets = []
+enemy_bullets = []
 last_shoot_time = 0
 shoot_delay = 100
-
-
 
 last_hit_time = 0
 hit_delay = 100
 
-baka = mixer.Sound("Fumo Hero/baka-cirno.mp3")
+baka = mixer.Sound("baka-cirno.mp3")
 
 class GameSprite(sprite.Sprite):    
     def __init__(self, player_image, playerX, playerY, player_speed):
@@ -51,7 +52,6 @@ class Player(GameSprite):
         global shoot_delay
         current_time = time.get_ticks()
         
-        
         keys = key.get_pressed()
         if keys[K_a] and self.rect.x > 5:
             self.rect.x -= self.speed
@@ -63,7 +63,6 @@ class Player(GameSprite):
             self.rect.y += self.speed
         
         if keys[K_SPACE] and current_time - last_shoot_time >= shoot_delay:
-            
             Bullet.create_bullet(self, allbullets)
             last_shoot_time = current_time
 
@@ -71,35 +70,28 @@ class Player(GameSprite):
         keys = key.get_pressed()
         if not keys[K_a] and not keys[K_d] and not keys[K_w] and not keys[K_s] and not keys[K_SPACE]:
             self.counter_idle += 0.05
-            self.image = transform.scale(image.load(f"Fumo Hero/{self.cirno_idle[int(self.counter_idle)]}"), (65, 65))
+            self.image = transform.scale(image.load(f"{self.cirno_idle[int(self.counter_idle)]}"), (65, 65))
                 
             if self.counter_idle >= len(self.cirno_idle) - 1:
                 self.counter_idle = 0
-        #if self.move_right():
-            #self.counter -= self.counter
-
-
 
     def move_right(self):
-            keys = key.get_pressed()
-            if keys[K_d]:
-                self.counter_right += 0.05
-                self.image = transform.scale(image.load(f"Fumo Hero/{self.cirno_right[int(self.counter_right)]}"), (65, 65))
-                    
-                if self.counter_right >= len(self.cirno_right) - 1:
-                    self.counter_right = 0
+        keys = key.get_pressed()
+        if keys[K_d]:
+            self.counter_right += 0.05
+            self.image = transform.scale(image.load(f"{self.cirno_right[int(self.counter_right)]}"), (65, 65))
+                
+            if self.counter_right >= len(self.cirno_right) - 1:
+                self.counter_right = 0
 
     def move_left(self):
-            keys = key.get_pressed()
-            if keys[K_a]:
-                self.counter_left += 0.05
-                self.image = transform.scale(image.load(f"Fumo Hero/{self.cirno_left[int(self.counter_left)]}"), (65, 65))
-                    
-                if self.counter_left >= len(self.cirno_left) - 1:
-                    self.counter_left = 0
-
-
-        
+        keys = key.get_pressed()
+        if keys[K_a]:
+            self.counter_left += 0.05
+            self.image = transform.scale(image.load(f"{self.cirno_left[int(self.counter_left)]}"), (65, 65))
+                
+            if self.counter_left >= len(self.cirno_left) - 1:
+                self.counter_left = 0
 
 class Bullet(sprite.Sprite):
     def __init__(self, bullet_image, bullet_x, bullet_y, speed):
@@ -109,7 +101,6 @@ class Bullet(sprite.Sprite):
         self.rect.x = bullet_x
         self.rect.y = bullet_y
         self.speed = speed
-        
 
     def draw_bullet(self):
         screen.blit(self.image, (self.rect.x, self.rect.y))
@@ -117,7 +108,7 @@ class Bullet(sprite.Sprite):
     direction = "up"
 
     def create_bullet(self, allbullets):
-        bullet = Bullet("Fumo Hero/Bullet.png", Fumo_destroyer.rect.x + 25, Fumo_destroyer.rect.y, 2)
+        bullet = Bullet("Bullet.png", Fumo_destroyer.rect.x + 25, Fumo_destroyer.rect.y, 2)
         allbullets.append(bullet)
 
     def update(self):
@@ -127,10 +118,30 @@ class Bullet(sprite.Sprite):
         if self.direction == "up":
             self.rect.y -= self.speed
 
+class EnemyBullet(sprite.Sprite):
+    def __init__(self, bullet_image, bullet_x, bullet_y, speed, angle=0):
+        super().__init__()
+        self.image = transform.scale(image.load(bullet_image), (20, 20))
+        self.rect = self.image.get_rect()
+        self.rect.x = bullet_x
+        self.rect.y = bullet_y
+        self.speed = speed
+        self.angle = angle
 
+    def draw_bullet(self):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+    def update(self):
+        self.rect.x += self.speed * math.cos(math.radians(self.angle))
+        self.rect.y += self.speed * math.sin(math.radians(self.angle))
+
+        if self.rect.y > win_height or self.rect.y < 0 or self.rect.x > win_width or self.rect.x < 0:
+            self.kill()
 
 class Enemy(GameSprite):
     direction = "down"
+    shoot_delay = 1000  # Delay between shots
+    last_shot_time = 0
 
     def update(self):
         if self.rect.y <= win_height + 70:
@@ -146,11 +157,76 @@ class Enemy(GameSprite):
         if self.rect.y > win_height:
             self.rect.y = Fumo_spawn_y
             self.rect.x = random.randint(10, 1070)
-            
 
+        # Shooting logic
+        current_time = time.get_ticks()
+        if current_time - self.last_shot_time >= self.shoot_delay:
+            self.shoot()
+            self.last_shot_time = current_time
 
-Fumo_destroyer = Player('Fumo Hero/Cirno0.png', (win_width / 2) - 65, win_height - 160, 4)
-Bad_Fumo = Enemy('Fumo Hero/Cirno9.webp', (win_width / 2) -50, 0, 1)
+    def shoot(self):
+        ### Cross pattern
+
+        # for angle in [0, 90, 180, 270]:
+        #     bullet = EnemyBullet("Fumo Hero/Bullet.png", self.rect.x + 25, self.rect.y + 25, 2, angle)
+        #     enemy_bullets.append(bullet)
+
+        # Additional patterns can be added here
+        # Example: Circle pattern
+
+        ### Spiral pattern
+
+        # num_bullets = 20  # Number of bullets in the spiral
+        # angle_step = 360 / num_bullets  # How much angle to increase for each bullet
+        
+        # for i in range(num_bullets):
+        #     angle = i * angle_step
+        #     bullet = EnemyBullet("Bullet.png", self.rect.x + 25, self.rect.y + 25, 2, angle)
+        #     enemy_bullets.append(bullet)
+
+        #angles = [45, 90, 135]  # Three angles forming a V-shape
+        #for angle in angles:
+        #    bullet = EnemyBullet("Bullet.png", self.rect.x + 25, self.rect.y + 25, 3, angle)
+        #    enemy_bullets.append(bullet)
+
+        ###Laser Beam pattern
+
+        # rows = 3
+        # cols = 5
+        # bullet_spacing = 50  # Distance between each bullet
+
+        # for i in range(rows):
+        #     for j in range(cols):
+        #         bullet_x = self.rect.x + (j - cols // 2) * bullet_spacing
+        #         bullet_y = self.rect.y + (i - rows // 2) * bullet_spacing
+        #         bullet = EnemyBullet("Bullet.png", bullet_x, bullet_y, 3,90)
+        #         enemy_bullets.append(bullet)
+
+        # bullet = EnemyBullet("Bullet.png", self.rect.x + 25, self.rect.y + 25, 5)
+        # bullet.angle = 90  # Straight down
+        # enemy_bullets.append(bullet)
+
+        ###Wave pattern !NOT WORKING!
+
+        # num_bullets = 10
+        # amplitude = 50  # Height of the wave
+        # frequency = 0.2  # How fast the wave oscillates
+
+        # for i in range(num_bullets):
+        #     angle = 90  # Downward direction
+        #     bullet = EnemyBullet("Bullet.png", self.rect.x + 25, self.rect.y + 25, 2, angle)
+        #     bullet.amplitude = amplitude
+        #     bullet.frequency = frequency
+        #     bullet.offset = i * 30  # Space out the bullets
+        #     enemy_bullets.append(bullet)
+        
+        # for bullet in enemy_bullets:
+        #     bullet.rect.x += math.sin(time.get_ticks() * bullet.frequency + bullet.offset) * bullet.amplitude
+        #     bullet.rect.y += bullet.speed
+        
+
+Fumo_destroyer = Player('Cirno0.png', (win_width / 2) - 65, win_height - 160, 4)
+Bad_Fumo = Enemy('Cirno9.webp', (win_width / 2) -50, 0, 1)
 
 clock = time.Clock()
 frames = 144
@@ -176,7 +252,6 @@ while running:
         Bad_Fumo.reset()
         Bad_Fumo.update()
         
-
         for i in allbullets:
             i.draw_bullet()
             if i.rect.y < 0:
@@ -187,8 +262,13 @@ while running:
                 if current_time - last_hit_time >= hit_delay:
                     last_hit_time = current_time
                     mixer.Sound.play(baka)
-                    
 
+        for bullet in enemy_bullets:
+            bullet.draw_bullet()
+            bullet.update()
+            if sprite.collide_rect(bullet, Fumo_destroyer):
+                enemy_bullets.remove(bullet)
+                # Handle player hit logic here
 
     display.update()
     clock.tick(frames)
